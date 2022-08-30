ba5d1cae5b4f9b6210f5ea82063b49a39a2c02fd
Guy Chamberlain-Webber <jchambgu@jci.com>
2022-08-30 09:30:35 +0100

    Documented the content header file

--- include/packet/content.h
+++ include/packet/content.h
@@ -14,18 +14,55 @@
 
 namespace Packets
 {
+    /**
+     * @brief A class specifying a packet contents object, which contains relevant functions
+     * to load a packet with data, and read from it.
+     */
     class Content : public IByteContainer
     {
         public:
             Content(std::vector<std::pair<std::string, unsigned char>>& params);
 
+            /**
+             * @brief Checks whether 'key' exists in the content parameters.
+             * 
+             * @param key The key to check for.
+             * @return true If 'key' exists.
+             * @return false If 'key' does not exist.
+             */
             bool check_exists(std::string key);
 
+            /**
+             * @brief Gets the object parameters as an array of bytes.
+             * 
+             * @return std::vector<unsigned char>& The object as an array of bytes.
+             */
             std::vector<unsigned char>& get_byte_array();
-            void set_parameter(std::string key, unsigned char value);
-            unsigned char get_parameter(std::string key);
-            std::vector<std::pair<std::string, unsigned char>>& get_parameters();
 
+            /**
+             * @brief Set the parameter contained at 'key' to 'value'.
+             * 
+             * @param key The key whose value is to be set.
+             * @param value The new value.
+             */
+            void set_parameter(std::string key, unsigned char value);
+
+            /**
+             * @brief Gets the parameter contained at 'key'.
+             * 
+             * @param key The key to whose value is to be returned.
+             * @return unsigned char The value contained at 'key'
+             */
+            unsigned char get_parameter(std::string key);
+
+            /**
+             * @brief Gets all the content parameters.
+             * 
+             * @return std::vector<std::pair<std::string, unsigned char>>&  The parameters of the
+             * content object.
+             */
+            std::vector<std::pair<std::string, unsigned char>>& get_parameters();
+        
             friend std::ostream& operator<<(std::ostream& os, Content const& v);
 
         protected:
