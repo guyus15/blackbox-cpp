4ab3bc634098c34db58c75d4f706f6c07d66ba03
Guy Chamberlain-Webber <jchambgu@jci.com>
2022-09-09 10:06:12 +0100

    Added multi-loop scanning in panel poll

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@
 #include "packet/packet_types.h"
 #include "clock.h"
 #include "config.h"
+#include "constants.h"
 #include "logger.h"
 
 #include <iostream>
 #include <vector>
 
 void send_test_packet(Logger& logger, std::string& logfile);
-std::vector<int> poll_points();
+std::vector<std::pair<int, int>> poll_points();
 
 int main()
 {
@@ -24,7 +25,7 @@ int main()
 
     send_test_packet(logger, logfile);
 
-    std::vector<int> points = poll_points();
+    std::vector<std::pair<int, int>> point_records = poll_points();
 
     bool should_run = true;
     float ping_time_period = Config::get_ping_time_period();
@@ -36,11 +37,14 @@ int main()
             continue;
         }
 
-        for (const auto& point : points)
+        for (const auto& point_record : point_records)
         {
-            std::cout << "Point: " << point << std::endl;
+            int loop_number = point_record.first;
+            int point_number = point_record.second;
 
-            Packets::Types::PointInformationRequestMX5 packet{point};
+            std::cout << "Loop: " << loop_number << "\nPoint: " << point_number << std::endl;
+
+            Packets::Types::PointInformationRequestMX5 packet{point_number, loop_number};
 
             packet.write();
 
@@ -67,7 +71,7 @@ int main()
 void send_test_packet(Logger& logger, std::string& logfile)
 {
     // Point informationr request for point 0.
-    Packets::Types::PointInformationRequestMX5 test_packet{0};
+    Packets::Types::PointInformationRequestMX5 test_packet{0, 1};
 
     test_packet.write();
 
@@ -84,29 +88,28 @@ void send_test_packet(Logger& logger, std::string& logfile)
 /**
  * @brief Looks through each of the points on the network and discovers those which actually exists.
  * 
- * @return std::vector<int> A vector of valid points on the network.
+ * @return std::vector<std::pair<int, int>> A vector of valid points on the network.
  */
-std::vector<int> poll_points()
+std::vector<std::pair<int, int>> poll_points()
 {
-    // NOTE: This needs some work, at the moment, valid points are only discovered on one loop,
-    // rather than network-wide. This will need to be updated in the future to scan the entire
-    // network.
-
-    std::vector<int> valid_points{};
+    std::vector<std::pair<int, int>> valid_points{};
 
     Clock clock{true};
 
     bool should_poll_points = true;
+
     float poll_time_period = Config::get_poll_time_period();
+
     int current_point_number = 0;
+    int current_loop_number = 1;
 
     while (should_poll_points)
     {
         if (clock.time_elapsed(poll_time_period))
         {
-            std::cout << "Polling point " << current_point_number << "..." << std::endl;
+            std::cout << "Polling point " << current_point_number << " on loop " << current_loop_number << "..." << std::endl;
 
-            Packets::Types::PointInformationRequestMX5 packet{current_point_number};
+            Packets::Types::PointInformationRequestMX5 packet{current_point_number, current_loop_number};
 
             packet.write();
 
@@ -114,12 +117,18 @@ std::vector<int> poll_points()
 
             if (read_data->reply_successful())
             {
-                valid_points.push_back(current_point_number);
+                valid_points.push_back({current_loop_number, current_point_number});
             }
 
             current_point_number++;
 
-            if (current_point_number > 250)
+            if (current_point_number > Constants::POINT_PER_LOOP)
+            {
+                current_point_number = 0;
+                current_loop_number++;
+            }
+
+            if (current_loop_number > Constants::MX5_MAX_LOOPS)
             {
                 should_poll_points = false;
             }
